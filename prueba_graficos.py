132b58535e2df831d4a84aff6c4b471eb21424d6
Nachokeso <167124110+IgnacioBerrios@users.noreply.github.com>
2024-11-22 11:43:56 -0300

    Update prueba_graficos.py

--- prueba_graficos.py
+++ prueba_graficos.py
@@ -31,11 +31,24 @@ filtered_data = filtered_data[
     (filtered_data['year'] >= rango_años[0]) & (filtered_data['year'] <= rango_años[1])
 ]
 
+color_map = { 
+    "R&B": "red",
+    "Electronic": "yellow",
+    "Pop": "blue",
+    "Folk": "green",
+    "Hip-Hop": "purple",
+    "Jazz": "orange",
+    "Classical":"brown",
+    "Country":"skyblue",
+    "Reggae":"white",
+    
+
 fig = px.scatter(
     filtered_data,
     x='release_date',
     y='stream',
     color='genre',
+    color_discrete_map=color_map,
     title=f"Fecha de Publicación vs Reproducciones ({selected_genre}, {rango_años[0]}-{rango_años[1]})",
     labels={"release_date": "Fecha de Publicación", "stream": "Reproducciones", "genre": "Genero"},
     template="plotly_white",
